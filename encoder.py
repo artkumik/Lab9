5d453b24ca12254c64da948b5b58d089e6db05f2
artkumik <artkumik@gmail.com>
2024-04-11 14:29:18 -0400

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,16 @@
+def main():
+    encode(12345555)
+
+def encode(user):
+    user = str(user)
+
+    encoding_list = [3,4,5,6,7,8,9,1,2]
+    encoded_user = ""
+
+    for i in user:
+        encoded_user = encoded_user + str(encoding_list[int(i)])
+
+    print(encoded_user)
+
+if __name__ == "__main__":
+    main()
