155e71254db8a01fca6f3455106d8d8f22f292bf
Dylan <160562249+ok-Mook@users.noreply.github.com>
2024-04-11 14:58:17 -0400

    Added decoder

--- encoder.py
+++ encoder.py
@@ -1,5 +1,6 @@
 def main():
     print(encode(12345555))
+    print(decode('45678888'))
 
 def encode(user):
     user = str(user)
@@ -11,6 +12,17 @@ def encode(user):
         encoded_user = encoded_user + str(encoding_list[int(i)])
 
     return encoded_user
+
+def decode(encoded_user):
+    lis = list(encoded_user)
+    decoded_user = ''
+    for i in range(len(lis)):
+        lis[i] = int(lis[i])
+        lis[i] = (lis[i] - 3) % 10
+    for i in range(len(lis)):
+        lis[i] = str(lis[i])
+        decoded_user += ''.join(lis[i])
+    return decoded_user
 
 if __name__ == "__main__":
     main()
